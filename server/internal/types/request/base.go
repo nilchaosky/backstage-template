5607097b808efd8d758934aa361ae6ac387a75a2
nilchaosky <84076179+nilchaosky@users.noreply.github.com>
2026-03-14 15:34:11 +0800

    Delete server directory

--- server/internal/types/request/base.go
+++
@@ -1,7 +0,0 @@
-package request
-
-// LoginRequest 登录请求
-type LoginRequest struct {
-	Username string `json:"username" binding:"required" label:"用户名"` // 用户名
-	Password string `json:"password" binding:"required" label:"密码"`  // 密码
-}
